8ed7c00faff7b9dee564de4a98c773a24ac55e25
rahulpalamut <rahulpalamut@gmail.com>
2014-04-05 14:24:13 -0700

    updated javascript file

--- web.js
+++ web.js
@@ -1,17 +1,35 @@
 //web.js
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
+// var express = require("express");
+// var logfmt = require("logfmt");
+// var app = express();
 
-app.use(logfmt.requestLogger());
+// app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
+// app.get('/', function(req, res) {
 
-  window.open(index.html);
+//   window.open(index.html);
+// });
+
+// var port = Number(process.env.PORT || 5000);
+// app.listen(port, function() {
+
+//    console.log("Listening on " + port);
+// });
+
+var sys = require('sys'),
+    http = require('http'),
+    fs = require('fs'),
+    index;
+ 
+fs.readFile('./index.html', function (err, data) {
+    if (err) {
+        throw err;
+    }
+    index = data;
 });
-
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-
-   console.log("Listening on " + port);
-});
+ 
+http.createServer(function(request, response) {
+    response.writeHeader(200, {"Content-Type": "text/html"});
+    response.write(index);
+    response.close();
+}).listen(8000);
