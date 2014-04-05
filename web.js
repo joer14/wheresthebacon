198486047882f45b11d3d4fcf5b27b485245cf11
rahulpalamut <rahulpalamut@gmail.com>
2014-04-05 16:16:29 -0700

    here

--- web.js
+++ web.js
@@ -10,12 +10,12 @@
 //     res.send('Hello World!');
 // });
 
-// var port = Number(process.env.PORT || 5000);
+//var port = Number(process.env.PORT || 5000);
 // app.listen(port, function() {
 
 //    console.log("Listening on " + port);
 // });
-
+// var port = Number(process.env.PORT || 5000);
 var sys = require('sys'),
     http = require('http'),
     fs = require('fs'),
@@ -27,7 +27,7 @@ fs.readFile('./index.html', function (err, data) {
     }
     index = data;
 });
- 
+console.log("Listening on " + port); 
 http.createServer(function(request, response) {
     response.writeHeader(200, {"Content-Type": "text/html"});
     response.write(index);
