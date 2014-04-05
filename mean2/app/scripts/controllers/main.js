65639cf2f5a9e22685b5d0b62f528133cd1221b8
endeav0ur <tylsmith@gmail.com>
2014-04-05 13:50:40 -0700

    nuked everything

--- mean2/app/scripts/controllers/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('meanApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-  });
