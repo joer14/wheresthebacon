336f6e67d93e1e1ea9e0bf1ee1291b738730d789
rahulpalamut <rahulpalamut@gmail.com>
2014-04-05 16:13:25 -0700

    please work

--- web.js
+++ web.js
@@ -1,35 +1,35 @@
 //web.js
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
+// var express = require("express");
+// var logfmt = require("logfmt");
+// var app = express();
 
-app.use(logfmt.requestLogger());
+// app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
+// app.get('/', function(req, res) {
 
-    res.send('Hello World!');
-});
-
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-
-   console.log("Listening on " + port);
-});
-
-// var sys = require('sys'),
-//     http = require('http'),
-//     fs = require('fs'),
-//     index;
- 
-// fs.readFile('./index.html', function (err, data) {
-//     if (err) {
-//         throw err;
-//     }
-//     index = data;
+//     res.send('Hello World!');
 // });
+
+// var port = Number(process.env.PORT || 5000);
+// app.listen(port, function() {
+
+//    console.log("Listening on " + port);
+// });
+
+var sys = require('sys'),
+    http = require('http'),
+    fs = require('fs'),
+    index;
  
-// http.createServer(function(request, response) {
-//     response.writeHeader(200, {"Content-Type": "text/html"});
-//     response.write(index);
-//     response.close();
-// }).listen(port);
+fs.readFile('./index.html', function (err, data) {
+    if (err) {
+        throw err;
+    }
+    index = data;
+});
+ 
+http.createServer(function(request, response) {
+    response.writeHeader(200, {"Content-Type": "text/html"});
+    response.write(index);
+    response.close();
+}).listen(port);
